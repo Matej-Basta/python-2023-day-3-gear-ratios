588fb2919a1c5e52017b0458a56bc0be7f593e65
matej <matej.basta@email.cz>
2024-07-02 12:05:29 +0200

    adding argument parser

--- firstChallenge.py
+++ firstChallenge.py
@@ -1,7 +1,23 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="This program solves day 3 challenge from Advent of Code 2023.")
+parser.add_argument("input_file", metavar="input_file", type=str, help="enter the input file")
+parser.add_argument("output_file", metavar="output_file", type=str, help="enter the output file")
+args = parser.parse_args()
+
 try:
-    with open("test.txt", "r") as input_file:
-        with open("output_file.txt", "w") as output_file:
-            print("here")
+    with open(args.input_file, "r") as input_file:
+        with open(args.output_file, "w") as output_file:
+            lines = []
+            for line in input_file:
+                lines.append(line.rstrip("\n"))
+            print(lines)
+            for index_line, line in enumerate(input_file):
+                for index_character, character in enumerate(line):
+                    if character.isdigit():
+                        previous_line_index = index_line - 1 if 0 <= index_line - 1 < len(lines) else None
+                        previous_character_index = line[index_character - 1] if 0 <= index_character - 1 < len(line) else None
+
 except FileNotFoundError:
     print("The file does not exist!")
 except:
