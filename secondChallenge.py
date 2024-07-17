715202b75e1a81d756368cbd70aa62958ff996a4
matej <matej.basta@email.cz>
2024-07-17 11:06:24 +0200

    second challenge done

--- secondChallenge.py
+++ secondChallenge.py
@@ -16,12 +16,45 @@ def main():
                 lines.insert(0, boundary_line)
                 lines.append(boundary_line)
 
-                
+                sum = 0
+                number_storage = {}
+                for line_index, line in enumerate(lines):
+                    is_in_number = False
+                    number = 0
+                    start_index = 0
+                    end_index = 0
+                    for index, ch in enumerate(line):
+                        if ch.isdigit():
+                            if not is_in_number:
+                                start_index = index
+                                is_in_number = True
+                            number = number*10 + int(ch)
+                        elif is_in_number:
+                            end_index = index - 1
+                            is_in_number = False
+                            if gear := get_adjacent_gear(line_index, start_index, end_index, lines):
+                                if prev_number := number_storage.get(gear):
+                                    sum += number * prev_number
+                                else:
+                                    number_storage[gear] = number
+                            number = 0
+                print(sum)
+                output_file.write(str(sum))
 
     except FileNotFoundError:
         print("File does not exist.")
     except:
         print("Something went wrong.")
+
+def is_gear(ch):
+    return ch == "*"
+
+def get_adjacent_gear(line_index, start_index, end_index, lines):
+    for i in range(line_index-1, line_index+2):
+        for j in range(start_index-1, end_index+2):
+            if is_gear(lines[i][j]):
+                return (i, j)
+    return None
 
 if __name__ == "__main__":
     main()
